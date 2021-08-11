0f532aaabb9f7b42dad6a3876f8d48198c30110f
bryaneagan <86554871+bryaneagan@users.noreply.github.com>
2021-08-11 21:25:15 +0700

    buat hero versi desktop

--- src/src/Hero.js
+++ src/src/Hero.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./Hero.css";
 
 const Hero = () => {
-  return <h1>Hero</h1>;
+  return (
+    <section className="section hero-section" id="home">
+      <div className="hero-container">
+        <h1>
+          Cari penyedia oksigen <br />
+          di daerah terdekatmu.
+        </h1>
+        <p>
+          Kami menggunakan data yang dikumpulkan oleh relawan #WargaBantuWarga.
+        </p>
+        <a href="#search" className="btn">
+          Cek sekarang
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
