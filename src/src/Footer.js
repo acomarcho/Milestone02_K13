1264baec3700bda78264eaeb7835ce9ec3743ff2
Dito <ammarditoshafaat2001@gmail.com>
2021-08-11 17:03:37 +0700

    nambahin footer buat desktop

--- src/src/Footer.js
+++ src/src/Footer.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Footer.css";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="footer" id="contact">
+      <div className="footer-container">
+        <h1 className="footer-logo">
+          Cari<span>Oksigen</span>.
+        </h1>
+        <div className="footer-contact">
+          <FaPhoneAlt />
+          <p>+62 812 2805 1776</p>
+        </div>
+        <p className="footer-info">
+          Data dikumpulkan oleh para relawan di #WargaBantuWarga.
+        </p>
+        <p className="footer-copyright">Copyright © 2021. Crafted with Love.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
