ec82cd3f56a3b95cbb73d2a425c0115a1304082f
Marchotridyo <acoxstpd@gmail.com>
2021-08-13 22:39:19 +0700

    made modal interactable

--- src/src/Modal.js
+++ src/src/Modal.js
@@ -1,31 +1,47 @@
 import React from "react";
 import "./Modal.css";
+import { FaTimes } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { ImLocation } from "react-icons/im";
+import { AiOutlineCheck } from "react-icons/ai";
+import copy from "copy-to-clipboard";
+// import ReactMapGL, { Marker } from "react-map-gl";
 
-// Kalau mau ngeshow modalnya kasih class model-overlay show
-const Modal = () => {
+const Modal = ({ state, dispatch }) => {
   return (
-    <div className="modal-overlay">
+    <div className={`modal-overlay ${state.showModal ? "show-modal" : null}`}>
       <div className="modal-card">
-        <button className="modal-close-btn">
+        <button
+          className="modal-close-btn"
+          onClick={() => dispatch({ type: "CLOSE_MODAL" })}
+        >
           <FaTimes />
         </button>
         <div className="modal-wrapper">
           <div className="modal-left">
-            <h1 className="modal-title">Nama</h1>
-            <p className="modal-subtitle">Deskripsi</p>
+            <h1 className="modal-title">{state.name}</h1>
+            <p className="modal-subtitle">{state.desc}</p>
             <div className="modal-contact">
               <FaPhoneAlt />
               <div className="modal-contact-info">
-                <p>Nomor</p>
-                <button className="modal-btn">Kontak</button>
+                <p>{state.phone}</p>
+                <button
+                  className="modal-btn"
+                  onClick={() => {
+                    copy(state.phone);
+                    alert("Nomor telah berhasil dicopy!");
+                  }}
+                >
+                  Kontak
+                </button>
               </div>
             </div>
             <div className="modal-location">
               <ImLocation />
               <div className="modal-location-info">
-                <p>Alamat</p>
+                <p>{state.alamat}</p>
                 <a
-                  href="/"
+                  href={`http://maps.google.com/maps?q=${state.latitude},${state.longitude}`}
                   target="_blank"
                   rel="noreferrer"
                   className="modal-btn"
@@ -34,28 +50,45 @@ const Modal = () => {
                 </a>
               </div>
             </div>
-            <div className="modal-verification">
-              <div className="modal-verification-icon">
-                <AiOutlineCheck />
+            {state.verified && (
+              <>
+                <div className="modal-verification">
+                  <div className="modal-verification-icon">
+                    <AiOutlineCheck />
+                  </div>
+                  <p>Verifikasi via {state.metode}</p>
+                </div>
+                <div className="modal-update">
+                  <p>Pembaruan terakhir {state.tanggal}</p>
+                </div>
+              </>
+            )}
+            {!state.verified && (
+              <div className="modal-update">
+                <p>Lokasi belum terverifikasi</p>
               </div>
-              <p>Verifikasi via Telepon</p>
-            </div>
-            <div className="modal-update">
-              <p>Pembaruan terakhir xx/yy/dd</p>
-            </div>
-            <button className="lapor-btn">Laporkan Kesalahan</button>
+            )}
+            <button
+              className="lapor-btn"
+              onClick={() => alert("Kesalahan terlaporkan!")}
+            >
+              Laporkan Kesalahan
+            </button>
           </div>
-          <div className="modal-right">
-            <iframe
-              frameBorder="0"
-              style={{ border: 0 }}
-              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD-tljClbOsofB_IM_1mzaLStiW0PL_9X8&q=1111,1111"
-              title="gmaps"
-            ></iframe>
-            <p>
-              1 km dari lokasi Anda <span>(1111, 1111)</span>
-            </p>
-          </div>
+          {state.latitude && (
+            <div className="modal-right">
+              <iframe
+                frameBorder="0"
+                style={{ border: 0 }}
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyD-tljClbOsofB_IM_1mzaLStiW0PL_9X8&q=${state.latitude},${state.longitude}`}
+                title="gmaps"
+              ></iframe>
+              <p>
+                {`${state.jarak} km dari lokasi Anda `}{" "}
+                <span>{`(${state.latitude}, ${state.longitude})`}</span>
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
